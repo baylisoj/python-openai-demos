8ab1a8e8aa8d87496f49967bde150a20df9f8f32
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2025-11-25 10:46:12 +0000

    Fix azure client to use AzureOpenAI

--- rag_csv_exposures.py
+++ rag_csv_exposures.py
@@ -16,9 +16,10 @@ if API_HOST == "azure":
     token_provider = azure.identity.get_bearer_token_provider(
         azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
     )
-    client = openai.OpenAI(
-        base_url=os.environ["AZURE_OPENAI_ENDPOINT"],
-        api_key=token_provider,
+    client = openai.AzureOpenAI(
+        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+        azure_ad_token_provider=token_provider,
+        api_version="2024-08-01-preview"
     )
     MODEL_NAME = os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"]
 
