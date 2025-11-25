42f5132e0f60af1bef227adfc2a26e2b78dc49cc
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2025-11-25 10:54:32 +0000

    Remove configuration for other api hosts other than azure

--- rag_csv_exposures.py
+++ rag_csv_exposures.py
@@ -6,34 +6,18 @@ import openai
 from dotenv import load_dotenv
 from lunr import lunr
 
-# Setup the OpenAI client to use either Azure, OpenAI.com, or Ollama API
+# Setup the Azure OpenAI client
 load_dotenv(override=True)
-API_HOST = os.getenv("API_HOST", "github")
 
-print(os.environ["API_HOST"])
-
-if API_HOST == "azure":
-    token_provider = azure.identity.get_bearer_token_provider(
-        azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-    )
-    client = openai.AzureOpenAI(
-        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-        azure_ad_token_provider=token_provider,
-        api_version="2024-08-01-preview"
-    )
-    MODEL_NAME = os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"]
-
-elif API_HOST == "ollama":
-    client = openai.OpenAI(base_url=os.environ["OLLAMA_ENDPOINT"], api_key="nokeyneeded")
-    MODEL_NAME = os.environ["OLLAMA_MODEL"]
-
-elif API_HOST == "github":
-    client = openai.OpenAI(base_url="https://models.github.ai/inference", api_key=os.environ["GITHUB_TOKEN"])
-    MODEL_NAME = os.getenv("GITHUB_MODEL", "openai/gpt-4o")
-
-else:
-    client = openai.OpenAI(api_key=os.environ["OPENAI_KEY"])
-    MODEL_NAME = os.environ["OPENAI_MODEL"]
+token_provider = azure.identity.get_bearer_token_provider(
+    azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+)
+client = openai.AzureOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_ad_token_provider=token_provider,
+    api_version="2024-08-01-preview"
+)
+MODEL_NAME = os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"]
 
 # Index the data from the CSV
 with open("exposures.csv") as file:
@@ -81,5 +65,5 @@ while True:
         ],
     )
     
-    print(f"\nResponse from {API_HOST}:\n")
+    print("\nResponse from Azure OpenAI:\n")
     print(response.choices[0].message.content)
